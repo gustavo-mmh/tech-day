05a7df9d4fadc2be7d1e8ab0bfedb8b7e9b632f4
gustavo-mmh <gustavo-mmello@hotmil.com>
2022-06-12 13:12:17 -0300

    update

--- backend/src/main/java/com/techday/techdaycarrefour/service/impl/UserServiceImpl.java
+++ backend/src/main/java/com/techday/techdaycarrefour/service/impl/UserServiceImpl.java
@@ -5,7 +5,9 @@ import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.hibernate.service.spi.ServiceException;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -20,9 +22,10 @@ public class UserServiceImpl implements IUserService {
 
 	@Autowired
 	private UserRepository repository;
-
+	@Autowired
+	private PasswordEncoder passwordEncoder;
 	@Override
-	public UserDTO create(UserForm form) {
+	public UserDTO create(@NotNull UserForm form) {
 		User user = repository.findByLogin(form.getLogin());
 		if (user != null) {
 			throw new ServiceException("Login já existe");
@@ -32,7 +35,7 @@ public class UserServiceImpl implements IUserService {
 		obj.setNome(form.getNome());
 		obj.setEmail(form.getEmail());
 		obj.setLogin(form.getLogin());
-		obj.setPassword(form.getPassword());
+		obj.setPassword(passwordEncoder.encode(form.getPassword()));
 		obj.setCep(form.getCep());
 		obj = repository.save(obj);
 		return new UserDTO(obj);
@@ -42,20 +45,19 @@ public class UserServiceImpl implements IUserService {
 	@Override
 	public UserDTO findById(Long id) {
 		User userEntity = repository.findById(id).get();
-		UserDTO dto = new UserDTO(userEntity);
-		return dto;
+		return new UserDTO(userEntity);
 	}
 
 	@Transactional
 	@Override
 	public UserDTO update(Long id, UserForm formUpdate) {
 		Optional<User> userData = repository.findById(id);
-		if (userData != null) {
+		if (userData.isPresent()) {
 			User obj = userData.get();
 			obj.setNome(formUpdate.getNome());
 			obj.setEmail(formUpdate.getEmail());
 			obj.setLogin(formUpdate.getLogin());
-			obj.setPassword(formUpdate.getPassword());
+			obj.setPassword(passwordEncoder.encode(formUpdate.getPassword()));
 			obj.setCep(formUpdate.getCep());
 			obj = repository.save(obj);
 			return new UserDTO(obj);
@@ -70,17 +72,17 @@ public class UserServiceImpl implements IUserService {
 	public List<UserDTO> getAll(String cep) {
 		if (cep == null) {
 			List<User> list = repository.findAll();
-			return list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+			return list.stream().map(UserDTO::new).collect(Collectors.toList());
 		} else {
 			List<User> listuserbycep = repository.findByCep(cep);
-			return listuserbycep.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+			return listuserbycep.stream().map(UserDTO::new).collect(Collectors.toList());
 		}
 	}
 
 	@Override
 	public void delete(Long id) {
 		Optional<User> userData = repository.findById(id);
-		if (userData != null) {
+		if (userData.isPresent()) {
 		repository.deleteById(id);
 		} else {
 			throw new ServiceException("Usuário não existe!");
